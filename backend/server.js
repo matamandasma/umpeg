4c2bf5591b0ee8a75c99753fd7a890c2cac0f602
randhyrs <randhyrahmans@gmail.com>
2026-04-28 14:13:57 +0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from "dotenv";
+import cookieParser from "cookies-parser";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
